5d2a71237ff513e35fe51abbf266a295396f80e8
edenbarker <eden.barker@mail.utoronto.ca>
2022-04-16 22:11:00 -0400

    new commit by eden

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -17,54 +17,54 @@ library(knitr)
 library(readr)
 
 # Read in the raw data for user1
-user1 <- read_csv("https://raw.githubusercontent.com/edenbarker/peloton_dataset/main/Peloton%20User%201.csv")
+userdata1 <- read.csv("https://raw.githubusercontent.com/edenbarker/peloton_dataset/main/Persona1.csv")
 View(user1)
 
 # Cleaning data names and shrinking data frame
 cleaned_user1 <-
-  clean_names(user1)
+  clean_names(userdata1)
 cleaned_user1
 
-user1df <- 
+userdata1 <- 
   cleaned_user1 %>% 
   select("date", "length_minutes", "instructor_name", "fitness_discipline",
          "type", "title", "total_output", "avg_watts", "avg_resistance", 
          "avg_cadence_rpm", "avg_speed_kph", "distance_km", "avg_heartrate") %>%
   na.omit(cleaned_user1) #removed NA rows
-view(user1df)
+view(userdata1)
 
 # Selecting rows where Fitness Discipline is equal to 'Cycling' as workout focus
-user1df[user1df$fitness_discipline == 'Cycling'] 
+userdata1[userdata1$fitness_discipline == 'Cycling'] 
 
 # Saving cleaned data
-write_csv(
-  x = user1df,
-  file = "user1df.csv")
+write.csv(
+  x = userdata1,
+  file = "persona_1.csv")
 
 
 # Read in the raw data for user2
-user2 <- read_csv("https://raw.githubusercontent.com/edenbarker/peloton_dataset/main/Peloton%20User%202.csv")
+userdata2 <- read.csv("https://raw.githubusercontent.com/edenbarker/peloton_dataset/main/Persona2.csv")
 View(user2)
 
 # Cleaning data names and shrinking data frame
 cleaned_user2 <-
-  clean_names(user2)
+  clean_names(userdata2 )
 cleaned_user2
 
-user2df <- 
+userdata2  <- 
   cleaned_user2 %>% 
   select("date", "length_minutes", "instructor_name", "fitness_discipline",
          "type", "title", "total_output", "avg_watts", "avg_resistance", 
          "avg_cadence_rpm", "avg_speed_kph", "distance_km", "avg_heartrate") %>%
   na.omit(cleaned_user2) #removed NA rows
-view(user2df)
+view(userdata2)
 
 # Selecting rows where Fitness Discipline is equal to 'Cycling' as workout focus
-user1df[user1df$fitness_discipline == 'Cycling']
+userdata2[userdata2$fitness_discipline == 'Cycling']
 
 # Saving cleaned data
-write_csv(
-  x = user1df,
-  file = "user2df.csv")
+write.csv(
+  x = userdata2,
+  file = "persona_2.csv")
 
 #### End of Data Cleaning  ####
